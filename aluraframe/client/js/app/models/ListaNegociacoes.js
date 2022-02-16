dcd63434aba17d0ae52343037c517a261068cc19
Ju Andrade <juliana.andraden07@gmail.com>
2022-02-16 10:37:29 -0300

    Browser e padroes

---
+++ aluraframe/client/js/app/models/ListaNegociacoes.js
@@ -0,0 +1,31 @@
+class ListaNegociacoes {
+
+    constructor() {
+
+        this._negociacoes = [];
+    }
+
+    adiciona(negociacao) {
+
+        this._negociacoes.push(negociacao);
+    }
+
+    get negociacoes() {
+
+        return [].concat(this._negociacoes);
+    }
+
+    esvazia() {
+
+        this._negociacoes = [];
+    }
+
+    ordena(criterio) {
+
+        this._negociacoes.sort(criterio);
+    }
+
+    get volumeTotal() {
+       return this._negociacoes.reduce((total, n) => total + n.volume, 0.0);
+    }
+}
